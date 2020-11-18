00245af6d5b3d98b67918e8ec7affd9eaf89c82f
Mohammad Allahham <mmallahham@hotmail.com>
2020-11-18 01:39:23 +0100

    use course 4 project as a starting point for the final project

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '3obd2irhta'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-s5id78ab.eu.auth0.com',
+  clientId: 'ilwsMeBabDDnjh0q4uvzmmCPHPPFScqy',
+  callbackUrl: 'http://localhost:3000/callback'
+}
